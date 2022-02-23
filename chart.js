5b2a57d4893a1ac319cc870838fca487dd2bc25d
jasonchen23 <kevin3121961@gmail.com>
2022-02-23 17:54:34 +0800

    add workshop

--- chart.js
+++ chart.js
@@ -17,7 +17,7 @@ const data1 = {
 const config1= {
     type: 'pie',
     data: data1,
-    options: {
+    options: { 
         maintainAspectRatio: false
     },
 };
@@ -84,6 +84,27 @@ const config3= {
             line: {
                 borderWidth: 3
             }
+        },
+        scales: {
+            r: {
+                angleLines: {
+                    color: 'black',
+                },
+                grid: {
+                    color: 'pink',
+                },
+                pointLabels: {
+                    color:'red',
+                    font: {
+                        size: 16
+                    }
+                },
+                ticks :{
+                    color: 'green',
+                },
+                min: 0,
+                max: 100
+            }
         }
     },
 };
@@ -119,3 +140,151 @@ const config4= {
     },
 };
 const chart4 = new Chart(ctx4,config4);
+
+const canvas5= document.getElementById('chart5');
+var ctx5 = canvas5.getContext('2d');
+const data5 = {
+    datasets: [{
+        label: 'Scatter Dataset',
+        data: [{
+            x: -10,
+            y: 0
+        }, {
+            x: 0,
+            y: 10
+        }, {
+            x: 10,
+            y: 5
+        }, {
+            x: 0.5,
+            y: 5.5
+        }],
+        backgroundColor:'rgb(255, 99, 132)',
+    }]
+};
+const config5= {
+    type: 'scatter',
+    data: data5,
+    options: {
+        maintainAspectRatio: false,
+        scales: {
+            x: {
+                type: 'linear',
+                position: 'bottom',
+            }
+        }
+    },
+};
+const chart5 = new Chart(ctx5,config5);
+
+const canvas6= document.getElementById('chart6');
+var ctx6 = canvas6.getContext('2d');
+const data6 = {
+    datasets: [{
+        label: 'First Dataset',
+        data: [{
+            x: 20,
+            y: 30,
+            r: 15
+        }, {
+            x: 40,
+            y: 10,
+            r: 10
+        },{
+            x: 30,
+            y: 15,
+            r: 6
+        }],
+        backgroundColor:'rgb(255, 99, 132)',
+    }]
+};
+const config6= {
+    type: 'bubble',
+    data: data6,
+    options: {
+        maintainAspectRatio: false,
+    },
+};
+const chart6 = new Chart(ctx6,config6);
+
+const canvas7= document.getElementById('chart7');
+var ctx7 = canvas7.getContext('2d');
+const data7 = {
+    labels: [
+        'January',
+        'February',
+        'March',
+        'April'
+    ],
+    datasets: [{
+        type: 'bar',
+        label: 'Bar Dataset',
+        data: [10, 20, 30, 40],
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.2)'
+    }, {
+        type: 'line',
+        label: 'Line Dataset',
+        data: [23, 12, 31, 50],
+        fill: false,
+        borderColor: 'rgb(54, 162, 235)'
+    }]
+};
+
+const config7= {
+    type: 'scatter',
+    data: data7,
+    options: {
+        maintainAspectRatio: false,
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    },
+};
+const chart7 = new Chart(ctx7,config7);
+
+const canvas8= document.getElementById('chart8');
+var ctx8 = canvas8.getContext('2d');
+const data8 = {
+    labels: [
+        'Red',
+        'Blue',
+        'Yellow'
+    ],
+    datasets: [{
+        label: 'My First Dataset',
+        data: [300, 50, 100],
+        backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+        ],
+        hoverOffset: 4
+    }]
+};
+
+const image = new Image();
+image.src = 'https://www.chartjs.org/img/chartjs-logo.svg';
+
+const plugin = {
+    id: 'custom_canvas_background_image',
+    beforeDraw: (chart) => {
+        if (image.complete) {
+            const ctx = chart.ctx;
+            const {top, left, width, height} = chart.chartArea;
+            const x = left + width / 2 - image.width / 2;
+            const y = top + height / 2 - image.height / 2;
+            ctx.drawImage(image, x, y);
+        } else {
+            image.onload = () => chart.draw();
+        }
+    }
+};
+const config8 = {
+    type: 'doughnut',
+    data: data8,
+    plugins: [plugin],
+};
+const chart8 = new Chart(ctx8,config8);
