8c5fb4d9437010aebe68a0cea1a2210dd3e61281
Jasonchen23 <kevin3121961@gmail.com>
2022-02-23 23:57:47 +0800

    add vue

---
+++ vue-chart/src/components/LineChart.vue
@@ -0,0 +1,10 @@
+<script>
+import { Line } from 'vue-chartjs'
+export default {
+    extends: Line,
+    props: ['chartData','options'],
+    mounted () {
+        this.renderChart(this.data, this.options);
+    }
+}
+</script>
---
+++ vue-chart/src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="home">
+      <LineChart :chartData="chartData" :options="options" />
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import LineChart from '@/components/LineChart.vue'
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld,
+    LineChart
+  },
+  data() {
+    return {
+      c
+    }
+  }
+}
+</script>
